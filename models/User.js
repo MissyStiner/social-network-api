51eae9c9573b2a9dff86e9b4127b66ea1bb4a783
Missy Stiner <missystiner@gmail.com>
2021-11-14 12:28:03 -0600

    server up and running

--- models/User.js
+++ models/User.js
@@ -1,23 +1,59 @@
+const { Schema, model } = require('mongoose');
 // User
-
+const userSchema = new Schema(
 // username
-
 // String
 // Unique
 // Required
 // Trimmed
+{
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+},
 // email
-
 // String
 // Required
 // Unique
 // Must match a valid email address (look into Mongoose's matching validation)
+email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [/.+@.+\..+/, 'Must match an email address!'],
+},
 // thoughts
-
 // Array of _id values referencing the Thought model
+thoughts: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Thought',
+    },
+],
 // friends
-
 // Array of _id values referencing the User model (self-reference)
+friends: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+    },
+  ],
+},
+{
+  toJSON: {
+    virtuals: true,
+  },
+  id: false,
+}
+);
 // Schema Settings
-
 // Create a virtual called friendCount that retrieves the length of the user's friends array field on query.
+userSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
+  });
+  
+  const User = model('User', userSchema);
+  
+  module.exports = User;
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,24 +1,37 @@
 // /api/users
-
 // GET all users
-
 // GET a single user by its _id and populated thought and friend data
-
 // POST a new user:
-
 // // example data
 // {
 //   "username": "lernantino",
 //   "email": "lernantino@gmail.com"
 // }
 // PUT to update a user by its _id
-
 // DELETE to remove user by its _id
-
 // BONUS: Remove a user's associated thoughts when deleted.
+// /api/users/:userId/friends/:friendId
+// POST to add a new friend to a user's friend list
+// DELETE to remove a friend from a user's friend list
+
+const router = require('express').Router();
+const {
+  getUsers,
+  getSingleUser,
+  createUser,
+  updateUser,
+  deleteUser,
+  addFriend,
+  removeFriend,
+} = require('../../controllers/user-controller');
+
+// /api/users
+router.route('/').get(getUsers).post(createUser);
+
+// /api/users/:userId
+router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser);
 
 // /api/users/:userId/friends/:friendId
+router.route('/:userId/friends/:friendId').post(addFriend).delete(removeFriend);
 
-// POST to add a new friend to a user's friend list
-
-// DELETE to remove a friend from a user's friend list
+module.exports = router;
