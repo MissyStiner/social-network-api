aa8f247f3e043798ed39de021af1c464a5fc6e64
Missy Stiner <missystiner@gmail.com>
2021-11-14 12:47:41 -0600

    gitignore updated

--- models/Thought.js
+++ models/Thought.js
@@ -1,22 +1,53 @@
 // Thought
-
+const { Schema, model } = require('mongoose');
+const reactionSchema = require('./Reaction');
+const dateFormat = require('../utils/dateFormat');
 // thoughtText
-
 // String
 // Required
 // Must be between 1 and 280 characters
+const thoughtSchema = new Schema(
+    {
+    thoughtText: {
+        type: String,
+        required: 'You need to leave a thought!',
+        minlength: 1,
+        maxlength: 280
+    },
 // createdAt
-
 // Date
 // Set default value to the current timestamp
 // Use a getter method to format the timestamp on query
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: timestamp => dateFormat(timestamp)
+    },
 // username (The user that created this thought)
-
 // String
 // Required
+    username: {
+        type: String,
+        required: true
+    },
 // reactions (These are like replies)
-
 // Array of nested documents created with the reactionSchema
+    reactions: [reactionSchema]
+    },
+    {
+        toJSON: {
+        getters: true
+    },
+        id: false
+    }
+    );
 // Schema Settings
-
 // Create a virtual called reactionCount that retrieves the length of the thought's reactions array field on query.
+thoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+  });
+  
+    const Thought = model('Thought', thoughtSchema);
+  
+    module.exports = Thought;
+  
