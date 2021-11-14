013a29964e751131349cc9c8dff50b5b69e53090
Missy Stiner <missystiner@gmail.com>
2021-11-14 12:58:38 -0600

    thoughts route and server running

--- models/Thought.js
+++ models/Thought.js
@@ -1,7 +1,8 @@
 // Thought
 const { Schema, model } = require('mongoose');
 const reactionSchema = require('./Reaction');
-const dateFormat = require('../utils/dateFormat');
+// const dateFormat = require('../utils/dateFormat');
+
 // thoughtText
 // String
 // Required
